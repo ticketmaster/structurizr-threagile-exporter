c2fd1ced9a800db569cc6b4742b4767fa23735da
Yevhen Zavhorodnii <yevhen.zavhorodnii@ticketmaster.co.uk>
2024-03-13 18:58:49 +0000

    Fix test

--- lib/src/test/java/com/ticketmaster/structurizr/threagile/ThreagileWorkspaceExporterTest.java
+++ lib/src/test/java/com/ticketmaster/structurizr/threagile/ThreagileWorkspaceExporterTest.java
@@ -5,10 +5,12 @@ import org.yaml.snakeyaml.Yaml;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Map;
 
 import com.structurizr.Workspace;
@@ -41,6 +43,13 @@ public class ThreagileWorkspaceExporterTest {
         });
     }
 
+    // TODO: move to a helper class
+    // those methods are required to check the yaml output is the same and decision to make custom check was made because standard assertEquals does not work with yaml
+    // issues with standard assertEquals:
+    // yaml may have different order of elements
+    // the same value may be represented in different ways (e.g. true/false vs yes/no)
+    // order of elements in lists may be different
+    // some fields are not important for the comparison
     void assertYamlEquals(String expectedYaml, String actualYaml, String message, String[] ignoreFields) {
         Yaml yaml = new Yaml();
         Map<String, Object> expected = yaml.load(expectedYaml);
@@ -49,7 +58,31 @@ public class ThreagileWorkspaceExporterTest {
         Map<String, Object> actual = yaml.load(actualYaml);
         excludeFields(actual, ignoreFields);
         
-        assertEquals(expected, actual, message);
+        assertMapEquals(expected, actual, message);
+    }
+
+    @SuppressWarnings("unchecked")
+    void assertMapEquals(Map<String, Object> expected, Map<String, Object> actual, String message) {
+        assertEquals(expected.size(), actual.size(), message + ": different size");
+
+        expected.forEach((key, value) -> {
+            if (value instanceof Map) {
+                assertMapEquals((Map<String, Object>) value, (Map<String, Object>) actual.get(key), message);
+            } else if (value instanceof ArrayList) {
+                ArrayList<Object> expectedList = (ArrayList<Object>) value;
+                ArrayList<Object> actualList = (ArrayList<Object>) actual.get(key);
+                assertEquals(expectedList.size(), actualList.size(), message + ": different list size");
+                for (int i = 0; i < expectedList.size(); i++) {
+                    if (expectedList.get(i) instanceof Map) {
+                        assertMapEquals((Map<String, Object>) expectedList.get(i), (Map<String, Object>) actualList.get(i), message);
+                    } else {
+                        assertTrue(actualList.contains(expectedList.get(i)), message);
+                    }
+                }
+            } else {
+                assertEquals(value, actual.get(key), message);
+            }
+        });
     }
 
     @SuppressWarnings("unchecked")
